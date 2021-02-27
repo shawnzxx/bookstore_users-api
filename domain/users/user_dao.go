059f1499d9fcfee035c1a7924d96fcb732e3e346
Zhang Xiaoxiao <shawnzhang.dev@gmail.com>
2021-02-27 22:46:30 +0800

    add date time support

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -3,6 +3,7 @@ package users
 import (
 	"fmt"
 
+	"github.com/shawnzxx/bookstore_users-api/utils/date"
 	"github.com/shawnzxx/bookstore_users-api/utils/errors"
 )
 
@@ -31,6 +32,9 @@ func (user *User) Save() *errors.RestErr {
 		}
 		return errors.NewBadRequestError(fmt.Sprintf("user %d is already excist", user.Id))
 	}
+
+	user.DateCreated = date.GetNowString()
+
 	dbUsers[user.Id] = user
 	return nil
 }
---
+++ utils/date/date_utils.go
@@ -0,0 +1,13 @@
+package date
+
+import "time"
+
+const dateTimeFormat = "2006-01-02T15:04:05Z"
+
+func GetNow() time.Time {
+	return time.Now().UTC()
+}
+
+func GetNowString() string {
+	return GetNow().Format(dateTimeFormat)
+}
