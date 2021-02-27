aba8779ad41330852c84c5d363907b07b54107c9
Zhang Xiaoxiao <shawnzhang.dev@gmail.com>
2021-02-27 11:16:59 +0800

    initial check with DDD structure

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,43 @@
+package users
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shawnzxx/bookstore_users-api/domain/users"
+	"github.com/shawnzxx/bookstore_users-api/services"
+	"github.com/shawnzxx/bookstore_users-api/utils/errors"
+)
+
+func CreateUser(c *gin.Context) {
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
+}
+
+func GetUser(c *gin.Context) {
+	userId, err := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if err != nil {
+		restErr := errors.NewBadRequestError("user id should be a number")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
---
+++ domain/users/user_dao.go
@@ -0,0 +1,36 @@
+package users
+
+import (
+	"fmt"
+
+	"github.com/shawnzxx/bookstore_users-api/utils/errors"
+)
+
+var dbUsers = make(map[int64]*User)
+
+func (user *User) Get() *errors.RestErr {
+	result := dbUsers[user.Id]
+	if result == nil {
+		return errors.NewBadRequestError(fmt.Sprintf("user %d not found", user.Id))
+	}
+
+	user.Id = result.Id
+	user.FirstName = result.FirstName
+	user.LastName = result.LastName
+	user.Email = result.Email
+	user.DateCreated = result.DateCreated
+
+	return nil
+}
+
+func (user *User) Save() *errors.RestErr {
+	current := dbUsers[user.Id]
+	if current != nil {
+		if current.Email == user.Email {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s is already registered", user.Email))
+		}
+		return errors.NewBadRequestError(fmt.Sprintf("user %d is already excist", user.Id))
+	}
+	dbUsers[user.Id] = user
+	return nil
+}
