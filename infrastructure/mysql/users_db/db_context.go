96656cf547fbe5bcb6400f641c4a69d3fb4f6e4c
Shaine <Shaine@tencent.com>
2021-09-18 19:51:58 +0800

    add keep on retry db connection this is for docker-compose depends not know when db is ready

--- infrastructure/mysql/users_db/db_context.go
+++ infrastructure/mysql/users_db/db_context.go
@@ -10,19 +10,19 @@ import (
 )
 
 const (
-	mysqlUsersUsername = "mysql_users_username"
-	mysqlUsersPassword = "mysql_users_password"
-	mysqlUsersHost     = "mysql_users_host"
-	mysqlUsersSchema   = "mysql_users_schema"
+	MysqlUsersUsername = "MYSQL_USERS_USERNAME"
+	MysqlUsersPassword = "MYSQL_USERS_PASSWORD"
+	MysqlUsersHost     = "MYSQL_USERS_HOST"
+	MysqlUsersSchema   = "MYSQL_USERS_SCHEMA"
 )
 
 //DbConn - export connected DbConn object
 var (
 	DbContext *sql.DB
-	username  = os.Getenv(mysqlUsersUsername)
-	password  = os.Getenv(mysqlUsersPassword)
-	host      = os.Getenv(mysqlUsersHost)
-	schema    = os.Getenv(mysqlUsersSchema)
+	username  = os.Getenv(MysqlUsersUsername)
+	password  = os.Getenv(MysqlUsersPassword)
+	host      = os.Getenv(MysqlUsersHost)
+	schema    = os.Getenv(MysqlUsersSchema)
 )
 
 //SetupDatabase - connect to the db
@@ -32,12 +32,28 @@ func SetupDatabase() {
 		username, password, host, schema)
 	DbContext, err = sql.Open("mysql", connectionString)
 	if err != nil {
-		panic(err)
+		log.Println(err.Error())
+	}
+	//Retry Database Connect With Docker and Go
+	//http://www.matthiassommer.it/programming/docker-compose-retry-database-connect-with-docker-and-go/
+	retryCount := 30
+	for {
+		err := DbContext.Ping()
+		if err != nil {
+			if retryCount == 0 {
+				log.Fatalf("Not able to establish connection to host %s database %s", host, schema)
+			}
+			log.Printf(fmt.Sprintf("Could not connect to database. Wait 2 seconds. %d retries left...", retryCount))
+			retryCount--
+			time.Sleep(2 * time.Second)
+		} else {
+			break
+		}
 	}
 	// test ping db after connect to the database
-	if err = DbContext.Ping(); err != nil {
-		panic(err)
-	}
+	//if err = DbContext.Ping(); err != nil {
+	//	panic(err)
+	//}
 	DbContext.SetMaxOpenConns(3)
 	DbContext.SetMaxIdleConns(3)
 	DbContext.SetConnMaxLifetime(60 * time.Second)
