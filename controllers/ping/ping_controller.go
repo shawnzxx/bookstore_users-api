e8e2660f5bfbd91a1771c58137444ce51c7009ec
Shaine <Shaine@tencent.com>
2021-09-19 00:13:49 +0800

    add keep on retry db connection this is for docker-compose depends not know when db is ready

--- controllers/ping/ping_controller.go
+++ controllers/ping/ping_controller.go
@@ -2,9 +2,11 @@ package ping
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/shawnzxx/bookstore_utils-go/logger"
 	"net/http"
 )
 
 func Ping(c *gin.Context) {
+	logger.Info("pong")
 	c.String(http.StatusOK, "pong")
 }
